6916d199400ebcc3e556a263c659ff930a7749dd
Akshat Pande <akshat.pande@outlook.com>
2021-06-10 22:34:15 +0530

    Update MainProject.py

--- MainProject.py
+++ MainProject.py
@@ -6,6 +6,8 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
 import youtube_dl
+import librosa
+import soundfile as sf
 import librosa.display
 from PIL import Image
 import pathlib
