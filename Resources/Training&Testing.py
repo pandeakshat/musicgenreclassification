6b531393631399634aec4031d00c5e9ea2a6c067
Akshat Pande <akshat.pande@outlook.com>
2021-06-14 14:30:17 +0530

    Create Training&Testing.py

---
+++ Resources/Training&Testing.py
@@ -0,0 +1,124 @@
+# Usual Libraries
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+import sklearn
+import librosa
+import librosa.display
+import IPython.display as ipd
+import warnings
+warnings.filterwarnings('ignore')
+import os
+general_path = '~/'
+from sklearn import preprocessing
+
+data = pd.read_csv(f'data.csv')
+data.head()
+
+
+data = data.iloc[0:, 1:]
+y = data['label']
+X = data.loc[:, data.columns != 'label']
+
+#### NORMALIZE X ####
+cols = X.columns
+min_max_scaler = preprocessing.MinMaxScaler()
+np_scaled = min_max_scaler.fit_transform(X)
+X = pd.DataFrame(np_scaled, columns = cols)
+
+
+#### PCA 2 COMPONENTS ####
+from sklearn.decomposition import PCA
+
+pca = PCA(n_components=2)
+principalComponents = pca.fit_transform(X)
+principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2'])
+
+# concatenate with target label
+finalDf = pd.concat([principalDf, y], axis = 1)
+
+pca.explained_variance_ratio_
+
+# 44.93 variance explained
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import SGDClassifier, LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from xgboost import XGBClassifier, XGBRFClassifier
+from xgboost import plot_tree, plot_importance
+
+from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score, roc_curve
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import RFE
+
+y = data['label'] # genre variable.
+X = data.loc[:, data.columns != 'label'] #select all columns but not the labels
+
+#### NORMALIZE X ####
+
+# Normalize so everything is on the same scale. 
+
+cols = X.columns
+min_max_scaler = preprocessing.MinMaxScaler()
+np_scaled = min_max_scaler.fit_transform(X)
+
+# new data frame with the new scaled data. 
+X = pd.DataFrame(np_scaled, columns = cols)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+
+
+def model_assess(model, title = "Default"):
+    model.fit(X_train, y_train)
+    preds = model.predict(X_test)
+    #print(confusion_matrix(y_test, preds))
+    print('Accuracy', title, ':', round(accuracy_score(y_test, preds), 5), '\n')
+
+# Naive Bayes
+nb = GaussianNB()
+model_assess(nb, "Naive Bayes")
+
+# Stochastic Gradient Descent
+sgd = SGDClassifier(max_iter=5000, random_state=0)
+model_assess(sgd, "Stochastic Gradient Descent")
+
+# KNN
+knn = KNeighborsClassifier(n_neighbors=19)
+model_assess(knn, "KNN")
+
+# Decission trees
+tree = DecisionTreeClassifier()
+model_assess(tree, "Decission trees")
+
+# Random Forest
+rforest = RandomForestClassifier(n_estimators=1000, max_depth=10, random_state=0)
+model_assess(rforest, "Random Forest")
+
+# Support Vector Machine
+svm = SVC(decision_function_shape="ovo")
+model_assess(svm, "Support Vector Machine")
+
+# Logistic Regression
+lg = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial')
+model_assess(lg, "Logistic Regression")
+
+# Neural Nets
+nn = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5000, 10), random_state=1)
+model_assess(nn, "Neural Nets")
+
+# Cross Gradient Booster
+xgb = XGBClassifier(n_estimators=1000, learning_rate=0.05)
+model_assess(xgb, "Cross Gradient Booster")
+
+# Cross Gradient Booster (Random Forest)
+xgbrf = XGBRFClassifier(objective= 'multi:softmax')
+model_assess(xgbrf, "Cross Gradient Booster (Random Forest)")
+
